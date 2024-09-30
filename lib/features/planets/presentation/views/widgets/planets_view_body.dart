38c213d221e3f7f06d5baae987ca0a4006f1f175
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-30 23:04:54 +0300

    back to stack

--- lib/features/planets/presentation/views/widgets/planets_view_body.dart
+++ lib/features/planets/presentation/views/widgets/planets_view_body.dart
@@ -32,104 +32,101 @@ class _PlanetsViewBodyState extends State<PlanetsViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-          child: Column(
-            children: [
-              vGap(15),
-              const CustomAppBar(),
-              vGap(18),
-              Text(
-                "Enjoy the amazing world beyond our solar system!",
-                style: AppTextStyles.font14GreyW400,
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Stack(
+        children: [
+          Positioned(
+            top: context.height * (500 / 812),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset(
+                AppAssets.halfCircle,
+                fit: BoxFit.fill,
               ),
-              vGap(20),
-              const CustomRichText(),
-              vGap(20),
-              BlocBuilder<PlanetsCubit, PlanetsState>(
-                builder: (context, state) {
-                  return PlanetInfoCard(
-                    title: planetData[context.read<PlanetsCubit>().currentIndex]
-                        .title,
-                    subtitle:
-                        planetData[context.read<PlanetsCubit>().currentIndex]
-                            .subtitle,
-                  );
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+            child: Column(
+              children: [
+                vGap(15),
+                const CustomAppBar(),
+                vGap(18),
+                Text(
+                  "Enjoy the amazing world beyond our solar system!",
+                  style: AppTextStyles.font14GreyW400,
+                ),
+                vGap(30),
+                const CustomRichText(),
+                vGap(30),
+                BlocBuilder<PlanetsCubit, PlanetsState>(
+                  builder: (context, state) {
+                    return PlanetInfoCard(
+                      title:
+                          planetData[context.read<PlanetsCubit>().currentIndex]
+                              .title,
+                      subtitle:
+                          planetData[context.read<PlanetsCubit>().currentIndex]
+                              .subtitle,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          // Positioned arrow image
+          Positioned(
+            left: context.width * 0.27,
+            top: context.height * 0.4,
+            child: Image.asset(AppAssets.anArrowPointingAtAPlanet),
+          ),
+          // Positioned PageView with defined size
+          Positioned(
+            top: context.height * 0.23,
+            left: context.width * 0.45,
+            child: SizedBox(
+              height: context.height * 0.65,
+              width: context.width * 0.5,
+              child: PlanetsPageView(
+                onPageChanged: (p0) {
+                  context.read<PlanetsCubit>().changePage(p0);
                 },
+                planetTwoClick: () {
+                  _nextPage();
+                },
+                pageController: _pageController,
+                angle: 50,
+                width: context.width * 0.5,
+                height: context.height * 0.18,
+                imagePaths: [
+                  planetData[0].image,
+                  planetData[1].image,
+                  planetData[2].image,
+                  planetData[3].image,
+                  planetData[4].image,
+                  planetData[5].image,
+                  planetData[6].image,
+                  planetData[7].image,
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-        vGap(20),
-        Expanded(
-          child: Stack(
-            children: [
-              Positioned(
-                top: context.height * (180 / 812),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset(
-                    AppAssets.halfCircle,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              // Positioned arrow image
-              Positioned(
-                left: context.width * 0.27,
-                child: Image.asset(AppAssets.anArrowPointingAtAPlanet),
-              ),
-              // Positioned PageView with defined size
-              Positioned(
-                bottom: context.height * 0.1,
-                left: context.width * 0.45,
-                child: SizedBox(
-                  height: context.height * 0.5,
-                  width: context.width * 0.5,
-                  child: PlanetsPageView(
-                    onPageChanged: (p0) {
-                      context.read<PlanetsCubit>().changePage(p0);
-                    },
-                    planetTwoClick: () {
-                      _nextPage();
-                    },
-                    pageController: _pageController,
-                    angle: 50,
-                    width: context.width * 0.5,
-                    height: context.height * 0.18,
-                    imagePaths: [
-                      planetData[0].image,
-                      planetData[1].image,
-                      planetData[2].image,
-                      planetData[3].image,
-                      planetData[4].image,
-                      planetData[5].image,
-                      planetData[6].image,
-                      planetData[7].image,
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 30.h),
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    height: 50.h,
-                    width: MediaQuery.of(context).size.width - 100.w,
-                    child: CustomTextButton(
-                        text: "Explore planet",
-                        onTap: () {},
-                        style: AppTextStyles.font20WhiteW500),
-                  ),
-                ),
-              ),
-            ],
+          Positioned(
+            top: 610.h,
+            left: 50,
+            child: SizedBox(
+              height: 50.h,
+              width: MediaQuery.of(context).size.width - 100,
+              child: CustomTextButton(
+                  text: "Explore planet",
+                  onTap: () {},
+                  style: AppTextStyles.font20WhiteW500),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
